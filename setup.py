afbfc557639cd3d5aa7378827e46a9008d2b665c
Grant <gvermillion@transportfoundry.com>
2020-06-13 15:56:53 -0700

    update author, email, and bump version

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ install_requires = [r.strip() for r in requirements]
 
 setuptools.setup(
     name='simfection',
-    version='0.1.0',
-    author='Grant Vermillion',
-    author_email='grant.cates@gmail.com',
+    version='0.1.1',
+    author='Simfection Research Group',
+    author_email='simfectiongroup@gmail.com',
     description='A stochastic, interaction-based infectious disease model',
     long_description=long_description,
     long_description_content_type='text/markdown',
