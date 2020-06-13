d7b3311640286df90295a56a9a7bf28c7a92233b
Grant <gvermillion@transportfoundry.com>
2020-06-13 10:22:54 -0700

    change to install_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ import setuptools
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+requirements = [
+    'pandas==1.0.4',
+    'PyYAML>=5.0.0',
+]
+install_requires = [r.strip() for r in requirements]
 
 setuptools.setup(
     name='simfection',
@@ -15,10 +20,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/simfection/simfection',
     packages=setuptools.find_packages(),
-    requirements=[
-        'pandas==1.0.4',
-        'PyYAML>=5.0.0',
-    ],
+    install_requires=install_requires,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
