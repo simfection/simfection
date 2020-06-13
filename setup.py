9a3b576bfa044e7099022cdedf498ce71d04b54f
Grant <gvermillion@transportfoundry.com>
2020-06-13 10:14:00 -0700

    add requirements explicitly and bump version

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ import setuptools
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    requirements = f.readlines()
 
 setuptools.setup(
     name='simfection',
-    version='0.0.4',
+    version='0.0.5',
     author='Grant Vermillion',
     author_email='grant.cates@gmail.com',
     description='A stochastic, interaction-based infectious disease model',
@@ -17,6 +15,10 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/simfection/simfection',
     packages=setuptools.find_packages(),
+    requirements=[
+        'pandas==1.0.4',
+        'PyYAML>=5.0.0',
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
