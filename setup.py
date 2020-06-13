d422f18875b820c4e87c005660cd774a43ab70fe
Grant <gvermillion@transportfoundry.com>
2020-06-13 09:43:57 -0700

    bump version and update url

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="simfection",
-    version="0.0.2",
+    version="0.0.3",
     author="Grant Vermillion",
     author_email="grant.cates@gmail.com",
     description="A stochastic, interaction-based infectious disease model",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gvermillion/simfection",
+    url="https://github.com/simfection/simfection",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
