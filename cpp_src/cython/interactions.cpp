1d9de62ce62a096b7dde6af3f7425a963a2b40eb
Alexander Ogle <alexander.ogle@gmail.com>
2020-06-16 21:07:36 -0700

    Setup basic data types. Added getter and setter methods for all private class variables.

--- cpp_src/cython/interactions.cpp
+++ cpp_src/cython/interactions.cpp
@@ -7,7 +7,126 @@
 // User defined libraries
 #include "interactions.h"
 
+
+//-------------------------------------------------------//
+// Define the Population Class
+//-------------------------------------------------------//
+Interactions::Population::Population(){
+    // Do nothing
+}
+
+Interactions::Population::Population(std::vector<int> newAgents,
+                                     std::vector<std::string> newStates,
+                                     std::vector<std::vector<int>> newInfectedByList,
+                                     std::vector<int> newDaysInfected,
+                                     std::vector<int> newImmunities){
+    this->agents = newAgents;
+    this-> states = newStates;
+    this->infectedByList = newInfectedByList;
+    this->daysInfected = newDaysInfected;
+    this->immunities = newImmunities;
+
+}
+
+// Getters
+std::vector<int> Interactions::Population::getAgents(){
+    return this->agents;
+}
+
+std::vector<std::string> Interactions::Population::getStates(){
+    return this->states;
+}
+
+std::vector<std::vector<int>> Interactions::Population::getInfectedByList(){
+    return this->infectedByList;
+}
+
+std::vector<int> Interactions::Population::getDaysInfected(){
+    return this->daysInfected;
+}
+
+std::vector<int> Interactions::Population::getImmunities(){
+    return this->immunities;
+}
+
+// Setters
+void Interactions::Population::setAgents(std::vector<int> newAgents){
+    this->agents = newAgents;
+}
+
+void Interactions::Population::setStates(std::vector<std::string> newStates){
+    this->states = newStates;
+}
+
+void Interactions::Population::setInfectedByList(std::vector<std::vector<int>> newInfectedByList){
+    this->infectedByList = newInfectedByList;
+}
+
+void Interactions::Population::setDaysInfected(std::vector<int> newDaysInfected){
+    this->daysInfected = newDaysInfected;
+}
+
+void Interactions::Population::setImmunities(std::vector<int> newImmunities){
+    this->immunities = newImmunities;
+}
+
+
+//-------------------------------------------------------//
+// Define the Connections Class
+//-------------------------------------------------------//
+Interactions::Connections::Connections(){
+    // Do nothing
+}
+
+Interactions::Connections::Connections(std::vector<int> newAgents, 
+                           std::vector<std::vector<int>> newConnectionsList, 
+                           std::vector<int> newNumConnections,
+                           std::vector<int> newMaxConnections){
+    this->agents = newAgents;
+    this->connectionsList = newConnectionsList;
+    this->numConnections = newNumConnections;
+    this->maxConnections = newMaxConnections;
+    // Do nothing
+}
+
+// Getters
+std::vector<int> Interactions::Connections::getAgents(){
+    return this->agents;
+}
+
+std::vector<std::vector<int>> Interactions::Connections::getConnectionsList(){
+    return this->connectionsList;
+}
+
+std::vector<int> Interactions::Connections::getNumConnections(){
+    return this->numConnections;
+}
+
+std::vector<int> Interactions::Connections::getMaxConnections(){
+    return this->maxConnections;
+}
+
+// Setters
+void Interactions::Connections::setAgents(std::vector<int> newAgents){
+    this->agents = newAgents;
+}
+
+void Interactions::Connections::setConnectionsList(std::vector<std::vector<int>> newConnectionsList){
+    this->connectionsList = newConnectionsList;
+}
+
+void Interactions::Connections::setNumConnections(std::vector<int> newNumConnections){
+    this->numConnections = newNumConnections;
+}
+
+void Interactions::Connections::setMaxConnections(std::vector<int> newMaxConnections){
+    this->maxConnections = newMaxConnections;
+}
+
+
+//-------------------------------------------------------//
 // Define the Interactions Class
+//-------------------------------------------------------//
 Interactions::Interactions(){
     // Do nothing
 }
@@ -34,47 +153,33 @@ void Interactions::initPopulation(std::vector<int> newAgents,
                                   newImmunities);
 }
 
-Population Interactions::interactAll(){
+std::vector<std::vector<int>> getUniqueConnections(Interactions::Connections thisConnections){
+    std::vector<std::vector<int>> interaction_pairs;
+    // Get connection lists that are > 0 in length/size
+
+    // Generate a vector of all pairs of agent_a : agent_b's from the connections list
+
+    // Get rid of all duplicate agent_a : agent_b pairs
+
+    // return a 2D vector of the interaction pairs
+    return interaction_pairs;
+}
+
+Interactions::Population Interactions::interactAll(){
+    Interactions::Population population;
     // Get all the unique connections
-    
+
+    // For each connection, interact them
+
+    // return an object or set some class variables that have getters defined which can be pulled from 
+    // in the Python code later to reconstruct a population DataFrame. 
+    return population;
 }
 
 
-// Define the Population Class
-Interactions::Population::Population(){
-    // Do nothing
-}
-
-Interactions::Population::Population(std::vector<int> newAgents,
-                                     std::vector<std::string> newStates,
-                                     std::vector<std::vector<int>> newInfectedByList,
-                                     std::vector<int> newDaysInfected,
-                                     std::vector<int> newImmunities){
-    this->agents = newAgents;
-    this-> states = newStates;
-    this->infectedByList = newInfectedByList;
-    this->daysInfected = newDaysInfected;
-    this->immunities = newImmunities;
-
-}
-
-
-// Define the Connections Class
-Interactions::Connections::Connections(){
-    // Do nothing
-}
-
-Interactions::Connections::Connections(std::vector<int> newAgents, 
-                           std::vector<std::vector<int>> newConnectionsList, 
-                           std::vector<int> newNumConnections,
-                           std::vector<int> newMaxConnections){
-    this->agents = newAgents;
-    this->connectionsList = newConnectionsList;
-    this->numConnections = newNumConnections;
-    this->maxConnections = newMaxConnections;
-    // Do nothing
-}
-
+//-------------------------------------------------------//
+// Main function for debugging and testing
+//-------------------------------------------------------//
 int main(){
     // values for connection list stub
     std::vector<int> newAgents = {0, 1};
--- cpp_src/cython/interactions.h
+++ cpp_src/cython/interactions.h
@@ -4,51 +4,82 @@
 #include <unordered_map>
 
 class Interactions {
-    // private:
+    public:
     class Population {
-        // private: 
-        public:
+        private: 
         std::vector<int> agents;
         std::vector<std::string> states;
         std::vector<std::vector<int>> infectedByList;
         std::vector<int> daysInfected;
         std::vector<int> immunities;
+
+        public:
         Population();
         Population(std::vector<int> newAgents,
                    std::vector<std::string> newStates,
                    std::vector<std::vector<int>> newInfectedByList,
                    std::vector<int> newDaysInfected,
                    std::vector<int> newImmunities);
+        // Getters
+        std::vector<int> getAgents();
+        std::vector<std::string> getStates();
+        std::vector<std::vector<int>> getInfectedByList();
+        std::vector<int> getDaysInfected();
+        std::vector<int> getImmunities();
+        // Setters
+        void setAgents(std::vector<int> newAgents);
+        void setStates(std::vector<std::string> newStates);
+        void setInfectedByList(std::vector<std::vector<int>> newInfectedByList);
+        void setDaysInfected(std::vector<int> newDaysInfected);
+        void setImmunities(std::vector<int> newImmunities);
     };
 
     class Connections {
-        // private:
-        public:
+        private:
         std::vector<int> agents;
         std::vector<std::vector<int>> connectionsList;
         std::vector<int> numConnections;
         std::vector<int> maxConnections;
+
+        public:
         Connections();
         Connections(std::vector<int> newAgents, 
                     std::vector<std::vector<int>> newConnectionsList, 
                     std::vector<int> newNumConnections,
                     std::vector<int> newMaxConnections);
+        // Getters
+        std::vector<int> getAgents();
+        std::vector<std::vector<int>> getConnectionsList();
+        std::vector<int> getNumConnections();
+        std::vector<int> getMaxConnections();
+        // Setters
+        void setAgents(std::vector<int> newAgents);
+        void setConnectionsList(std::vector<std::vector<int>> newConnectionsList);
+        void setNumConnections(std::vector<int> newNumConnections);
+        void setMaxConnections(std::vector<int> newMaxConnections);
     };
-
-    public:
-    Population population;
-    Connections connections;
+    // Constructor
     Interactions();
+    // Setter for population
     void initPopulation(std::vector<int> newAgents,
                         std::vector<std::string> newStates,
                         std::vector<std::vector<int>> newInfectedByList,
                         std::vector<int> newDaysInfected,
                         std::vector<int> newImmunities);
+    // Setter for connections
     void initConnections(std::vector<int> newAgents, 
                          std::vector<std::vector<int>> newConnectionsList, 
                          std::vector<int> newNumConnections,
                          std::vector<int> newMaxConnections);
+    // Getters
+    Interactions::Population getPopulation();
+    Interactions::Connections getConections();
+    std::vector<std::vector<int>> getUniqueConnections(Connections thisConnections);
     Population interactAll();
+
+    private:
+    Population population;
+    Connections connections;
 
 };
 
